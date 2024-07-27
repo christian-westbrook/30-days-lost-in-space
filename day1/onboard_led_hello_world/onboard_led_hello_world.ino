a3af08f669fdefeae18783fa3a57b555a2e34768
christian-westbrook <christianwestbrook@live.com>
2024-07-27 13:11:35 -0500

    Added an updated solution to day 1 of the challenge

--- day1/onboard_led_hello_world/onboard_led_hello_world.ino
+++ day1/onboard_led_hello_world/onboard_led_hello_world.ino
@@ -1,200 +1,39 @@
-// -----------------------------------------------------------------------------
-// Script   : onboard_led_hello_world.ino
+// ============================================================================
+// Sketch   : onboard_led_hello_world.ino
 // Engineer : Christian Westbrook
-// Abstract : This script causes an Arduino Uno's onboard LED to
-//            repeat the message 'HELLO WORLD' using Morse code.
-// -----------------------------------------------------------------------------
+// Abstract : Blink onboard LED sporadically.
+//
+//            This work is derivative of the Blink.ino sketch by
+//            Scott Fitzgerald, Arturo Guadalupi, and Colby Newman.
+//
+//            https://github.com/arduino/arduino-examples/blob/main/examples/01.Basics/Blink/Blink.ino
+//
+// ============================================================================
 
-#define base_delay 200
-
-// Define delay values as multiples of the base delay
-int dit_delay = base_delay;
-int dah_delay = base_delay * 3;
-
-int space_between_units_delay = base_delay;
-int space_between_letters_delay = base_delay * 3;
-int space_between_words_delay = base_delay * 7;
-
-// ---------------------------- Arduino Interface ------------------------------
-// -----------------------------------------------------------------------------
+// ============================================================================
 // Function : setup()
 // Engineer : Christian Westbrook
-// Abstract : This function is called a single time as the Arduino is powered on
-//            or reset. This function initializes the onboard LED for use as an
-//            output device.
-// -----------------------------------------------------------------------------
+// Abstract : Initialize the onboard LED as an output
+// ============================================================================
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // Initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-// -----------------------------------------------------------------------------
+// ============================================================================
 
-// -----------------------------------------------------------------------------
+// ============================================================================
 // Function : loop()
 // Engineer : Christian Westbrook
-// Abstract : This function is called repeatedly after the setup() function exits.
-// -----------------------------------------------------------------------------
+// Abstract : Blink the onboard LED with alternating fast and slow blinks
+// ============================================================================
 void loop() {
-
-  // H - dit dit dit dit
-  dit();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_letters();
-
-  // E - dit
-  dit();
-  between_letters();
-
-  // L - dit dah dit dit
-  dit();
-  between_units();
-  dah();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_letters();
-
-  // L - dit dah dit dit
-  dit();
-  between_units();
-  dah();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_letters();
-
-  // O - dah dah dah
-  dah();
-  between_units();
-  dah();
-  between_units();
-  dah();
-  between_words();
-
-  // W - dit dah dah
-  dit();
-  between_units();
-  dah();
-  between_units();
-  dah();
-  between_letters();
-
-  // O - dah dah dah
-  dah();
-  between_units();
-  dah();
-  between_units();
-  dah();
-  between_letters();
-
-  // R - dit dah dit
-  dit();
-  between_units();
-  dah();
-  between_units();
-  dit();
-  between_letters();
-
-  // L - dit dah dit dit
-  dit();
-  between_units();
-  dah();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_letters();
-
-  // D - dah dit dit
-  dah();
-  between_units();
-  dit();
-  between_units();
-  dit();
-  between_words();
-}
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-
-// ---------------------------- Private Interface ------------------------------
-// -----------------------------------------------------------------------------
-// Function : dit()
-// Engineer : Christian Westbrook
-// Abstract : This function implements an LED signal pattern that represents
-//            a 'dit' in Morse code.
-// -----------------------------------------------------------------------------
-void dit() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(dit_delay);
-}
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Function : dah()
-// Engineer : Christian Westbrook
-// Abstract : This function implements an LED signal pattern that represents
-//            a 'dah' in Morse code.
-// -----------------------------------------------------------------------------
-void dah() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(dah_delay);
-}
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Function   : space()
-// Engineer   : Christian Westbrook
-// Parameters : int space_delay - The amount of time in milliseconds for which
-//              the onboard LED should maintain a LOW signal representing a
-//              space between elements in a Morse code message.
-//
-// Abstract   : This function implements an LED signal pattern that represents
-//              a space between elements of a message in Morse code. The input
-//              delay value is used to determine the length of the delay pattern
-//              in milliseconds.
-// -----------------------------------------------------------------------------
-void space(int space_delay) {
+  delay(3000);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(space_delay);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Function : between_units()
-// Engineer : Christian Westbrook
-// Abstract : This function implements an LED signal pattern that represents
-//            the space between units in a letter in Morse code.
-// -----------------------------------------------------------------------------
-void between_units() {
-  space(space_between_units_delay);
-}
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Function : between_letters()
-// Engineer : Christian Westbrook
-// Abstract : This function implements an LED signal pattern that represents
-//            the space between letters in Morse code.
-// -----------------------------------------------------------------------------
-void between_letters() {
-  space(space_between_letters_delay);
-}
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Function : between_words()
-// Engineer : Christian Westbrook
-// Abstract : This function implements an LED signal pattern that represents
-//            the space between words in Morse code.
-// -----------------------------------------------------------------------------
-void between_words() {
-  space(space_between_words_delay);
-}
-// -----------------------------------------------------------------------------
+// ============================================================================
